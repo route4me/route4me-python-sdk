17764dd13e41acd4f6f64a9f6af9b656d92a18ee
Juan Jose Pimentel <juanjosepimentel@gmail.com>
2015-03-01 12:09:32 -0500

    [MOD] API Endpoints

--- route4me/api_endpoints.py
+++ route4me/api_endpoints.py
@@ -1,9 +1,10 @@
-DRIVER_VERSION = "route4me-python-driver-0.0.1"
-MAIN_HOST = "http://www.route4me.com"
-API_HOST = MAIN_HOST + "/api.v4/optimization_problem.php"
-SHOW_ROUTE_HOST = MAIN_HOST + "/route4me.php"
-ROUTE_HOST = MAIN_HOST + "/api.v4/route.php"
-SET_GPS_HOST = MAIN_HOST + "/track/set.php"
-GEOCODER = MAIN_HOST + "/api/geocoder.php"
-EXPORTER = MAIN_HOST + "/actions/route/export_current_route.php"
+DRIVER_VERSION = 'route4me-python-driver-0.0.1'
+MAIN_HOST = 'https://www.route4me.com'
+API_HOST = '{0}/api.v4/optimization_problem.php'.format(MAIN_HOST)
+SHOW_ROUTE_HOST = '{0}/route4me.php'.format(MAIN_HOST)
+ROUTE_HOST = '{0}/api.v4/route.php'.format(MAIN_HOST)
+SET_GPS_HOST = '{0}/track/set.php'.format(MAIN_HOST)
+BATCH_GEOCODER = '{0}/api/geocoder.php'.format(MAIN_HOST)
+SINGLE_GEOCODER = '{0}/api/address.php'.format(MAIN_HOST) 
+EXPORTER = MAIN_HOST + '{0}/actions/route/export_current_route.php'
 
