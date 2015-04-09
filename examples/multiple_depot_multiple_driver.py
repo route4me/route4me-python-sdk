e8c0565892106f5f9346e972957486bce414b02e
Juan Jose Pimentel <juanjosepimentel@gmail.com>
2015-04-08 23:42:58 -0500

    [UPDATE] Multiple depot multiple driver example

--- examples/multiple_depot_multiple_driver.py
+++ examples/multiple_depot_multiple_driver.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import json
+
 from route4me import Route4Me
 from route4me.constants import *
 
@@ -23,134 +25,456 @@ def main():
     optimization.device_type(DEVICE_TYPE.WEB)
     optimization.travel_mode(TRAVEL_MODE.DRIVING)
     optimization.metric(METRIC.ROUTE4ME_METRIC_GEODESIC)
+
     address.add_address(
-        address='3634 W Market St, Fairlawn, OH 44333',
-        lat=41.135762259364,
-        lng=-81.629313826561,
-        is_depot=1,
-        time=300,
-        time_window_start=28800,
-        time_window_end=29465
+            address="455 S 4th St, Louisville, KY 40202",
+            lat=38.251698,
+            lng=-85.757308,
+            is_depot=1,
+            time=300,
+            time_window_start=28800,
+            time_window_end=29400
     )
     address.add_address(
-        address='1218 Ruth Ave, Cuyahoga Falls, OH 44221',
-        lat=41.143505096435,
-        lng=-81.46549987793,
-        time=300,
-        time_window_start=29465,
-        time_window_end=30529
+            address="1604 PARKRIDGE PKWY, Louisville, KY, 40214",
+            lat=38.141598,
+            lng=-85.793846,
+            is_depot=1,
+            time=300,
+            time_window_start=29400,
+            time_window_end=30000
     )
     address.add_address(
-        address='512 Florida Pl, Barberton, OH 44203',
-        lat=41.003671512008,
-        lng=-81.598461046815,
-        time=300,
-        time_window_start=30529,
-        time_window_end=33779
+            address="1407 MCCOY, Louisville, KY, 40215",
+            lat=38.202496,
+            lng=-85.786514,
+            is_depot=0,
+            time=300,
+            time_window_start=30000,
+            time_window_end=30600
     )
     address.add_address(
-        address='512 Florida Pl, Barberton, OH 44203',
-        lat=41.003671512008,
-        lng=-81.598461046815,
-        time=300,
-        time_window_start=33779,
-        time_window_end=33944
+            address="4805 BELLEVUE AVE, Louisville, KY, 40215",
+            lat=38.178844,
+            lng=-85.774864,
+            is_depot=0,
+            time=300,
+            time_window_start=30600,
+            time_window_end=31200
     )
     address.add_address(
-        address='3495 Purdue St, Cuyahoga Falls, OH 44221',
-        lat=41.162971496582,
-        lng=-81.479049682617,
-        time=300,
-        time_window_start=33944,
-        time_window_end=34801
+            address="730 CECIL AVENUE, Louisville, KY, 40211",
+            lat=38.248684,
+            lng=-85.821121,
+            is_depot=0,
+            time=300,
+            time_window_start=31200,
+            time_window_end=31800
     )
     address.add_address(
-        address='1659 Hibbard Dr, Stow, OH 44224',
-        lat=41.194505989552,
-        lng=-81.443351581693,
-        time=300,
-        time_window_start=34801,
-        time_window_end=36366
+            address="650 SOUTH 29TH ST UNIT 315, Louisville, KY, 40211",
+            lat=38.251923,
+            lng=-85.800034,
+            is_depot=0,
+            time=300,
+            time_window_start=31800,
+            time_window_end=32400
     )
     address.add_address(
-        address='2705 N River Rd, Stow, OH 44224',
-        lat=41.145240783691,
-        lng=-81.410247802734,
-        time=300,
-        time_window_start=36366,
-        time_window_end=39173
+            address="4629 HILLSIDE DRIVE, Louisville, KY, 40216",
+            lat=38.176067,
+            lng=-85.824638,
+            is_depot=0,
+            time=300,
+            time_window_start=32400,
+            time_window_end=33000
     )
     address.add_address(
-        address='10159 Bissell Dr, Twinsburg, OH 44087',
-        lat=41.340042114258,
-        lng=-81.421226501465,
-        time=300,
-        time_window_start=39173,
-        time_window_end=41617
+            address="4738 BELLEVUE AVE, Louisville, KY, 40215",
+            lat=38.179806,
+            lng=-85.775558,
+            is_depot=0,
+            time=300,
+            time_window_start=33000,
+            time_window_end=33600
     )
     address.add_address(
-        address='367 Cathy Dr, Munroe Falls, OH 44262',
-        lat=41.148578643799,
-        lng=-81.429229736328,
-        time=300,
-        time_window_start=41617,
-        time_window_end=43660
+            address="318 SO. 39TH STREET, Louisville, KY, 40212",
+            lat=38.259335,
+            lng=-85.815094,
+            is_depot=0,
+            time=300,
+            time_window_start=33600,
+            time_window_end=34200
     )
     address.add_address(
-        address='367 Cathy Dr, Munroe Falls, OH 44262',
-        lat=41.148579,
-        lng=-81.42923,
-        time=300,
-        time_window_start=43660,
-        time_window_end=46392
+            address="1324 BLUEGRASS AVE, Louisville, KY, 40215",
+            lat=38.179253,
+            lng=-85.785118,
+            is_depot=0,
+            time=300,
+            time_window_start=34200,
+            time_window_end=34800
     )
     address.add_address(
-        address='512 Florida Pl, Barberton, OH 44203',
-        lat=41.003671512008,
-        lng=-81.598461046815,
-        time=300,
-        time_window_start=46392,
-        time_window_end=48389
+            address="7305 ROYAL WOODS DR, Louisville, KY, 40214",
+            lat=38.162472,
+            lng=-85.792854,
+            is_depot=0,
+            time=300,
+            time_window_start=34800,
+            time_window_end=35400
     )
     address.add_address(
-        address='559 W Aurora Rd, Northfield, OH 44067',
-        lat=41.315116882324,
-        lng=-81.558746337891,
-        time=300,
-        time_window_start=48389,
-        time_window_end=48449
+            address="1661 W HILL ST, Louisville, KY, 40210",
+            lat=38.229584,
+            lng=-85.783966,
+            is_depot=0,
+            time=300,
+            time_window_start=35400,
+            time_window_end=36000
     )
     address.add_address(
-        address='3933 Klein Ave, Stow, OH 44224',
-        lat=41.169467926025,
-        lng=-81.429420471191,
-        time=300,
-        time_window_start=48449,
-        time_window_end=50152
+            address="3222 KINGSWOOD WAY, Louisville, KY, 40216",
+            lat=38.210606,
+            lng=-85.822594,
+            is_depot=0,
+            time=300,
+            time_window_start=36000,
+            time_window_end=36600
     )
     address.add_address(
-        address='2148 8th St, Cuyahoga Falls, OH 44221',
-        lat=41.136692047119,
-        lng=-81.493492126465,
-        time=300,
-        time_window_start=50152,
-        time_window_end=51982
+            address="1922 PALATKA RD, Louisville, KY, 40214",
+            lat=38.153767,
+            lng=-85.796783,
+            is_depot=0,
+            time=300,
+            time_window_start=36600,
+            time_window_end=37200
     )
     address.add_address(
-        address='3731 Osage St, Stow, OH 44224',
-        lat=41.161357879639,
-        lng=-81.42293548584,
-        time=300,
-        time_window_start=51982,
-        time_window_end=52180
+            address="1314 SOUTH 26TH STREET, Louisville, KY, 40210",
+            lat=38.235847,
+            lng=-85.796852,
+            is_depot=0,
+            time=300,
+            time_window_start=37200,
+            time_window_end=37800
     )
     address.add_address(
-        address='3731 Osage St, Stow, OH 44224',
-        lat=41.161357879639,
-        lng=-81.42293548584,
-        time=300,
-        time_window_start=52180,
-        time_window_end=54379
+            address="2135 MCCLOSKEY AVENUE, Louisville, KY, 40210",
+            lat=38.218662,
+            lng=-85.789032,
+            is_depot=0,
+            time=300,
+            time_window_start=37800,
+            time_window_end=38400
+    )
+    address.add_address(
+            address="1409 PHYLLIS AVE, Louisville, KY, 40215",
+            lat=38.206154,
+            lng=-85.781387,
+            is_depot=0,
+            time=300,
+            time_window_start=38400,
+            time_window_end=39000
+    )
+    address.add_address(
+            address="4504 SUNFLOWER AVE, Louisville, KY, 40216",
+            lat=38.187511,
+            lng=-85.839149,
+            is_depot=0,
+            time=300,
+            time_window_start=39000,
+            time_window_end=39600
+    )
+    address.add_address(
+            address="2512 GREENWOOD AVE, Louisville, KY, 40210",
+            lat=38.241405,
+            lng=-85.795059,
+            is_depot=0,
+            time=300,
+            time_window_start=39600,
+            time_window_end=40200
+    )
+    address.add_address(
+            address="5500 WILKE FARM AVE, Louisville, KY, 40216",
+            lat=38.166065,
+            lng=-85.863319,
+            is_depot=0,
+            time=300,
+            time_window_start=40200,
+            time_window_end=40800
+    )
+    address.add_address(
+            address="3640 LENTZ AVE, Louisville, KY, 40215",
+            lat=38.193283,
+            lng=-85.786201,
+            is_depot=0,
+            time=300,
+            time_window_start=40800,
+            time_window_end=41400
+    )
+    address.add_address(
+            address="1020 BLUEGRASS AVE, Louisville, KY, 40215",
+            lat=38.17952,
+            lng=-85.780037,
+            is_depot=0,
+            time=300,
+            time_window_start=41400,
+            time_window_end=42000
+    )
+    address.add_address(
+            address="123 NORTH 40TH ST, Louisville, KY, 40212",
+            lat=38.26498,
+            lng=-85.814156,
+            is_depot=0,
+            time=300,
+            time_window_start=42000,
+            time_window_end=42600
+    )
+    address.add_address(
+            address="7315 ST ANDREWS WOODS CIRCLE UNIT 104, Louisville, KY, 40214",
+            lat=38.151072,
+            lng=-85.802867,
+            is_depot=0,
+            time=300,
+            time_window_start=42600,
+            time_window_end=43200
+    )
+    address.add_address(
+            address="3210 POPLAR VIEW DR, Louisville, KY, 40216",
+            lat=38.182594,
+            lng=-85.849937,
+            is_depot=0,
+            time=300,
+            time_window_start=43200,
+            time_window_end=43800
+    )
+    address.add_address(
+            address="4519 LOUANE WAY, Louisville, KY, 40216",
+            lat=38.1754,
+            lng=-85.811447,
+            is_depot=0,
+            time=300,
+            time_window_start=43800,
+            time_window_end=44400
+    )
+    address.add_address(
+            address="6812 MANSLICK RD, Louisville, KY, 40214",
+            lat=38.161839,
+            lng=-85.798279,
+            is_depot=0,
+            time=300,
+            time_window_start=44400,
+            time_window_end=45000
+    )
+    address.add_address(
+            address="1524 HUNTOON AVENUE, Louisville, KY, 40215",
+            lat=38.172031,
+            lng=-85.788353,
+            is_depot=0,
+            time=300,
+            time_window_start=45000,
+            time_window_end=45600
+    )
+    address.add_address(
+            address="1307 LARCHMONT AVE, Louisville, KY, 40215",
+            lat=38.209663,
+            lng=-85.779816,
+            is_depot=0,
+            time=300,
+            time_window_start=45600,
+            time_window_end=46200
+    )
+    address.add_address(
+            address="434 N 26TH STREET #2, Louisville, KY, 40212",
+            lat=38.26844,
+            lng=-85.791962,
+            is_depot=0,
+            time=300,
+            time_window_start=46200,
+            time_window_end=46800
+    )
+    address.add_address(
+            address="678 WESTLAWN ST, Louisville, KY, 40211",
+            lat=38.250397,
+            lng=-85.80629,
+            is_depot=0,
+            time=300,
+            time_window_start=46800,
+            time_window_end=47400
+    )
+    address.add_address(
+            address="2308 W BROADWAY, Louisville, KY, 40211",
+            lat=38.248882,
+            lng=-85.790421,
+            is_depot=0,
+            time=300,
+            time_window_start=47400,
+            time_window_end=48000
+    )
+    address.add_address(
+            address="2332 WOODLAND AVE, Louisville, KY, 40210",
+            lat=38.233579,
+            lng=-85.794257,
+            is_depot=0,
+            time=300,
+            time_window_start=48000,
+            time_window_end=48600
+    )
+    address.add_address(
+            address="1706 WEST ST. CATHERINE, Louisville, KY, 40210",
+            lat=38.239697,
+            lng=-85.783928,
+            is_depot=0,
+            time=300,
+            time_window_start=48600,
+            time_window_end=49200
+    )
+    address.add_address(
+            address="1699 WATHEN LN, Louisville, KY, 40216",
+            lat=38.216465,
+            lng=-85.792397,
+            is_depot=0,
+            time=300,
+            time_window_start=49200,
+            time_window_end=49800
+    )
+    address.add_address(
+            address="2416 SUNSHINE WAY, Louisville, KY, 40216",
+            lat=38.186245,
+            lng=-85.831787,
+            is_depot=0,
+            time=300,
+            time_window_start=49800,
+            time_window_end=50400
+    )
+    address.add_address(
+            address="6925 MANSLICK RD, Louisville, KY, 40214",
+            lat=38.158466,
+            lng=-85.798355,
+            is_depot=0,
+            time=300,
+            time_window_start=50400,
+            time_window_end=51000
+    )
+    address.add_address(
+            address="2707 7TH ST, Louisville, KY, 40215",
+            lat=38.212438,
+            lng=-85.785082,
+            is_depot=0,
+            time=300,
+            time_window_start=51000,
+            time_window_end=51600
+    )
+    address.add_address(
+            address="2014 KENDALL LN, Louisville, KY, 40216",
+            lat=38.179394,
+            lng=-85.826668,
+            is_depot=0,
+            time=300,
+            time_window_start=51600,
+            time_window_end=52200
+    )
+    address.add_address(
+            address="612 N 39TH ST, Louisville, KY, 40212",
+            lat=38.273354,
+            lng=-85.812012,
+            is_depot=0,
+            time=300,
+            time_window_start=52200,
+            time_window_end=52800
+    )
+    address.add_address(
+            address="2215 ROWAN ST, Louisville, KY, 40212",
+            lat=38.261703,
+            lng=-85.786781,
+            is_depot=0,
+            time=300,
+            time_window_start=52800,
+            time_window_end=53400
+    )
+    address.add_address(
+            address="1826 W. KENTUCKY ST, Louisville, KY, 40210",
+            lat=38.241611,
+            lng=-85.78653,
+            is_depot=0,
+            time=300,
+            time_window_start=53400,
+            time_window_end=54000
+    )
+    address.add_address(
+            address="1810 GREGG AVE, Louisville, KY, 40210",
+            lat=38.224716,
+            lng=-85.796211,
+            is_depot=0,
+            time=300,
+            time_window_start=54000,
+            time_window_end=54600
+    )
+    address.add_address(
+            address="4103 BURRRELL DRIVE, Louisville, KY, 40216",
+            lat=38.191753,
+            lng=-85.825836,
+            is_depot=0,
+            time=300,
+            time_window_start=54600,
+            time_window_end=55200
+    )
+    address.add_address(
+            address="359 SOUTHWESTERN PKWY, Louisville, KY, 40212",
+            lat=38.259903,
+            lng=-85.823463,
+            is_depot=0,
+            time=300,
+            time_window_start=55200,
+            time_window_end=55800
+    )
+    address.add_address(
+            address="2407 W CHESTNUT ST, Louisville, KY, 40211",
+            lat=38.252781,
+            lng=-85.792109,
+            is_depot=0,
+            time=300,
+            time_window_start=55800,
+            time_window_end=56400
+    )
+    address.add_address(
+            address="225 S 22ND ST, Louisville, KY, 40212",
+            lat=38.257616,
+            lng=-85.786658,
+            is_depot=0,
+            time=300,
+            time_window_start=56400,
+            time_window_end=57000
+    )
+    address.add_address(
+            address="1404 MCCOY AVE, Louisville, KY, 40215",
+            lat=38.202122,
+            lng=-85.786072,
+            is_depot=0,
+            time=300,
+            time_window_start=57000,
+            time_window_end=57600
+    )
+    address.add_address(
+            address="117 FOUNT LANDING CT, Louisville, KY, 40212",
+            lat=38.270061,
+            lng=-85.799438,
+            is_depot=0,
+            time=300,
+            time_window_start=57600,
+            time_window_end=58200
+    )
+    address.add_address(
+            address="5504 SHOREWOOD DRIVE, Louisville, KY, 40214",
+            lat=38.145851,
+            lng=-85.7798,
+            is_depot=0,
+            time=300,
+            time_window_start=58200,
+            time_window_end=58800
     )
     print optimization.data
 
@@ -160,6 +484,8 @@ def main():
         print 'Route %s link: %sroute_id=%s' % (address.address,
                                                 route4me.route_url(),
                                                 address.route_id)
+    print json.dumps(route4me.response.content, ensure_ascii=False,
+                     sort_keys=True, indent=4)
     route4me.export_result_to_json('multiple_depot_multiple_driver.json')
 
 
