845322b57b5f512e423bc464c7b73408d8822929
Juan Jose Pimentel <juanjosepimentel@gmail.com>
2016-11-23 20:31:09 -0500

    [Fix] Bug

--- route4me/api_endpoints.py
+++ route4me/api_endpoints.py
@@ -1,6 +1,5 @@
 DRIVER_VERSION = 'route4me-python-driver-0.0.1'
-MAIN_HOST = 'http://www.broono.com'
-#MAIN_HOST = 'https://www.route4me.com'
+MAIN_HOST = 'https://www.route4me.com'
 
 ACTIVITY_FEED = '{0}/api.v4/activity_feed.php'.format(MAIN_HOST)
 ADDRESSBOOK = '{0}/api.v4/address_book.php'.format(MAIN_HOST)
