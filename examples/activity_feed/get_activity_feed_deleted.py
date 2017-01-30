ad1f3987e8f7dca04c99f0198d2aaa7c844167b0
Amazos <olegi_g@yahoo.com>
2017-01-30 17:47:53 +0400

    updated get_activity_feed_deleted.py file for codebeat issue

--- examples/activity_feed/get_activity_feed_deleted.py
+++ examples/activity_feed/get_activity_feed_deleted.py
@@ -10,15 +10,11 @@ def main():
     if hasattr(response, 'errors'):
         print '. '.join(response.errors)
     else:
-        print 'Total Found: {}'.format(response.total)
+        print 'Total affected: {}'.format(response.total)
         for i, activity in enumerate(response.results):
             print 'Activity #{}'.format(i + 1)
             print '\tActivity ID: {}'.format(activity.activity_id)
-            print '\tActivity Message: {}'.format(activity.activity_message)
             print '\tActivity Type: {}'.format(activity.activity_type)
-            print '\tRoute ID: {}'.format(activity.route_id)
-            print '\tRoute Name: {}'.format(activity.route_name)
-
 
 if __name__ == '__main__':
     main()
