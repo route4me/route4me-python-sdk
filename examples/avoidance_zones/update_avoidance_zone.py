c2be70ad187d852212477399422ff794fb03c9e5
Amazos <olegi_g@yahoo.com>
2017-01-31 17:19:59 +0400

    updated files for codebeat issues

--- examples/avoidance_zones/update_avoidance_zone.py
+++ examples/avoidance_zones/update_avoidance_zone.py
@@ -1,8 +1,8 @@
+# codebeat:disable[SIMILARITY]
 from route4me import Route4Me
 from route4me.constants import *
 
 KEY = "11111111111111111111111111111111"
-
 
 def main():
     route4me = Route4Me(KEY)
@@ -62,3 +62,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+# codebeat:enable[SIMILARITY]
