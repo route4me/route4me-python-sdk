281ee1573576aff17ed427446553f8925351ba90
maxkoryukov <maxkoryukov@yandex.ru>
2017-08-13 02:46:13 +0500

    ci: appveyor 22

---
+++ route4me/sdk/version.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 1, 0)
+VERSION_SUFFIX = '-dev'
+
+_v = map(str, VERSION)
+VERSION_STRING = '.'.join(_v[0:3]) + VERSION_SUFFIX
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-import os
+# -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup
+
+from route4me.sdk.version import VERSION_STRING
 
 
 def read(file_name):
@@ -11,20 +14,65 @@ def read(file_name):
 
 
 setup(
-    name="Route4Me Python SDK",
-    version="0.0.1",
-    author="Route4Me",
-    author_email="juan@route4me.com",
-    description=("Route4Me Python SDK"),
-    license="ISC",
-    keywords="rout4me, python, sdk, api",
-    url="http://route4me.com/api/demo",
-    packages=['route4me', 'examples', 'tests', ],
-    long_description=read('README.md'),
+    name='route4me-sdk',
+    version=VERSION_STRING,
+    url='https://github.com/route4me/route4me-python-sdk',   # ???
+    license='ISC',                                           # ???
+    author='Route4Me Python Team',
+    author_email='python-team@route4me.com',
+    description=('Route4Me Python SDK'),
+    long_description=read('README.rst'),
+    keywords='route4me, python, sdk, api',                   # ???
+    packages=['route4me.sdk', 'route4me.sdk.examples'],      # ???
+
+    # include_package_data=True,
+    # zip_safe=False,
+    # platforms='any',
+
     classifiers=[
-        "Development Status :: 1 - Beta",
-        "Topic :: SDK",
-        "License :: ISC",
+        'Development Status :: 4 - Beta',
+        # 'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: SDK',
+        'License :: ISC',
+        # 'License :: OSI Approved :: BSD License',
+        # 'Operating System :: OS Independent',
+        # 'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3.6',
+        # 'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        # 'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    test_suite="tests",
+    test_suite='tests',
+
+    install_requires=[
+        'Werkzeug>=0.9',
+        'Jinja2>=2.4',
+        'itsdangerous>=0.21',
+        'click>=4.0',
+    ],
+    extras_require={
+        'dotenv': ['python-dotenv'],
+        'dev': [
+            'blinker',
+            'python-dotenv',
+            'greenlet',
+            'pytest>=3',
+            'coverage',
+            'tox',
+            'sphinx',
+            'sphinxcontrib-log-cabinet'
+        ],
+    },
+    entry_points='''
+        [console_scripts]
+        flask=flask.cli:main
+    '''
+
 )
