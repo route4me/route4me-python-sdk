4e080756c32edef7c2500576b9544dc14255bdff
maxkoryukov <maxkoryukov@yandex.ru>
2017-08-24 01:24:54 +0500

    package: read and write version info to the package
    fix route4me/route4me-python-sdk#9

--- setup.py
+++ setup.py
@@ -2,71 +2,94 @@
 
 import os
 from setuptools import setup
+from setuptools import find_packages
 
-from route4me.sdk import __release__ as RELEASE
-from route4me.sdk import __title__ as TITLE
-from route4me.sdk import __copyright__ as COPYRIGHT
-from route4me.sdk import __author__ as AUTHOR
-from route4me.sdk import __project__ as PROJECT
+from VERSION import PROJECT
+from VERSION import COPYRIGHT
+from VERSION import AUTHOR
+from VERSION import TITLE
+from VERSION import LICENSE
+from VERSION import RELEASE_STRING
 
 cwd = os.path.dirname(__file__)
 
 
 def read_all(file_name):
-    fullname = os.path.join(cwd, file_name)
-    with open(fullname) as f:
-        return f.read()
+	fullname = os.path.join(cwd, file_name)
+	with open(fullname) as f:
+		return f.read()
+
+
+def rewrite_version():
+	with open('VERSION.py', 'r') as inp:
+		txt = inp.read()
+	outname = os.path.join('route4me', 'sdk', 'version.py')
+	with open(outname, 'w') as out:
+		out.write(txt)
+
+
+rewrite_version()
 
 
 setup(
-    name=TITLE,
-    version=RELEASE,
-    url='https://github.com/route4me/route4me-python-sdk',
-    bugtrack_url='https://github.com/route4me/route4me-python-sdk/issues',
-    license='ISC',                                           # ???
-    copyright=COPYRIGHT,
-    author=AUTHOR,
-    author_email='python-team@route4me.com',
-    description=PROJECT,
-    long_description=read_all('README.rst'),
-    keywords='route4me, python, sdk, api',                   # ???
-    packages=['route4me.sdk', 'route4me.sdk.examples'],      # ???
-    platforms='any',
+	name=TITLE,
+	version=RELEASE_STRING,
+	url='https://github.com/route4me/route4me-python-sdk',
+	bugtrack_url='https://github.com/route4me/route4me-python-sdk/issues',
+	license=LICENSE,
+	copyright=COPYRIGHT,
+	author=AUTHOR,
+	author_email='python-team@route4me.com',
+	description=PROJECT,
+	long_description=read_all('README.rst'),
+	keywords='route4me, python, sdk, api',
+	packages=find_packages(
+		include=['route4me.sdk', 'route4me.sdk.*'],
+		exclude=['*_test*'],
+	),
+	zip_safe=True,
 
-    # include_package_data=True,
-    # zip_safe=False,
+	platforms='any',
+	classifiers=[
+		# 'Development Status :: 1 - Planning',
+		# 'Development Status :: 2 - Pre-Alpha',
+		# 'Development Status :: 3 - Alpha',
+		# 'Development Status :: 4 - Beta',
+		# 'Development Status :: 5 - Production/Stable',
+		'Development Status :: 2 - Pre-Alpha',
+		'Intended Audience :: Developers',
+		'Environment :: Other Environment',
+		'License :: OSI Approved :: ISC License (ISCL)',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: Implementation',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Programming Language :: Python :: Implementation :: PyPy',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	],
+	test_suite='pytest',
 
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Developers',
-        'Environment :: Other Environment',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-    test_suite='pytest',
+	install_requires=[
+		'six             ==1.10.0',
+		'requests        ==2.18.4',
+		'enum34          ==1.1.6',
+		'pydash          ==4.1.0',
+	],
+	# include_package_data=True,
 
-    install_requires=[
-        'six             ==1.10.0',
-        'requests        ==2.18.4',
-        'enum34          ==1.1.6',
-    ],
-    # extras_require={
-    #     'dev': REQUIREMENTS_DEV,
-    # },
-    # entry_points='''
-    #     [console_scripts]
-    #     flask=flask.cli:main
-    # '''
+	# extras_require={
+	#     'dev': REQUIREMENTS_DEV,
+	# },
+
+	# entry_points='''
+	#     [console_scripts]
+	#     flask=flask.cli:main
+	# '''
 )
