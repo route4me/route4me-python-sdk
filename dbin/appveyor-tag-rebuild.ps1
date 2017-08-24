10f1bf1ab205c063205ba963eeb896b139b3d8b4
maxkoryukov <maxkoryukov@yandex.ru>
2017-08-24 20:19:34 +0500

    ci: appveyor 10

--- dbin/appveyor-tag-rebuild.ps1
+++ dbin/appveyor-tag-rebuild.ps1
@@ -1,16 +1,16 @@
 # restart builds for tag
 
-if ($Env:APPVEYOR_REPO_TAG -eq "true") {
-	if ($Env:CUSTOM_BUILD_TAG -eq "true") {
-		echo "Build tag with custom tunings"
-	} else {
-		echo "Trying to build tag without custom tunings"
-		Start-AppveyorBuild `
-			-ApiKey $env:CUSTOM_API_KEY `
-			-ProjectSlug 'route4me-python-sdk' `
-			-Branch $Env:APPVEYOR_REPO_TAG_NAME `
-			-EnvironmentVariables @{ CUSTOM_BUILD_TAG='true' }
+# if ($Env:APPVEYOR_REPO_TAG -eq "true") {
+# 	if ($Env:CUSTOM_BUILD_TAG -eq "true") {
+# 		echo "Build tag with custom tunings"
+# 	} else {
+# 		echo "Trying to build tag without custom tunings"
+# 		Start-AppveyorBuild `
+# 			-ApiKey $env:CUSTOM_API_KEY `
+# 			-ProjectSlug 'route4me-python-sdk' `
+# 			-Branch $Env:APPVEYOR_REPO_TAG_NAME `
+# 			-EnvironmentVariables @{ CUSTOM_BUILD_TAG='true' }
 
-		Exit-AppveyorBuild
-	}
-}
+# 		Exit-AppveyorBuild
+# 	}
+# }
