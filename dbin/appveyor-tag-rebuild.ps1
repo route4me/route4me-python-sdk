ac5fed1ebaa0934543fe17c4b45faee37a35f274
maxkoryukov <maxkoryukov@yandex.ru>
2017-08-24 19:39:11 +0500

    ci: test appveyor

---
+++ dbin/appveyor-tag-rebuild.ps1
@@ -0,0 +1,17 @@
+# restart builds for tag
+
+if ($Env:APPVEYOR_REPO_TAG == "true");
+then
+	if ($Env:CUSTOM_BUILD_TAG -eq "true");
+	then
+		echo "Trying to build tag without custom tunings"
+	else
+		Start-AppveyorBuild \
+			-ApiKey $env:CUSTOM_API_KEY
+			-ProjectSlug 'route4me-python-sdk'
+			-Branch $Env:APPVEYOR_REPO_TAG_NAME
+			-EnvironmentVariables @{ CUSTOM_BUILD_TAG='true' }
+
+		Exit-AppveyorBuild
+	fi
+fi
