d8b917f2ebd6ba54ab39f580d847a551fadf84e4
Tommy Bird <tommy@route4me.com>
2023-06-02 11:59:08 -0400

    Added clean_dict function

--- route4me/utils.py
+++ route4me/utils.py
@@ -26,3 +26,30 @@ def json2obj(data):
     :return: object
     """
     return json.loads(data, object_hook=_json_object_hook)
+
+
+def clean_dict(data):
+    """
+    Clean dictionary values
+    :param data: input dictionary
+    :return: cleaned dictionary
+    """
+    if isinstance(data, list):
+        cleaned_list = []
+        for i in data:
+            cleaned_item = clean_dict(i)
+            # Append non-None items
+            if cleaned_item not in [None, ""]:
+                cleaned_list.append(cleaned_item)
+        return cleaned_list if cleaned_list else None
+    elif isinstance(data, dict):
+        cleaned_dict = {}
+        for k, v in data.items():
+            cleaned_v = clean_dict(v)
+            # Append non-None key-value pairs
+            if cleaned_v not in [None, ""]:
+                cleaned_dict[k] = cleaned_v
+        return cleaned_dict if cleaned_dict else None
+    else:
+        # For non-container type, return as is
+        return data
